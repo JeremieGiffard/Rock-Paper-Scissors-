194e3d17933d0d2f7b685609ed142e15b80d235e
jeremiegiffard <e132784f@gmail.com>
2021-10-14 13:29:27 +0200

    Update loo

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+let playerScore = 0;
+let computerScore = 0;
+let playerSelection;
+
 // 1- function that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. 
 function computerPlay()
 {
@@ -6,9 +10,6 @@ function computerPlay()
     let computerChoice = choices[positionArray];
     return computerChoice ;
 }
-const computerSelection = computerPlay();
-//console.log(`${typeof computerSelection} ${computerSelection}`);
-
 
 
 // 2- function that ask player to choose either ‘Rock’, ‘Paper’ or ‘Scissors’. 
@@ -17,17 +18,18 @@ function playerPlay()
     let getChoice = prompt("Please select either Rock , Paper ou Scissors");
     // 2.1- Make function’s parameter case-insensitive
         // make first letter uper case
-        let firstLetter = getChoice.slice(0,1).toUpperCase()
+        let firstLetter = getChoice.slice(0,1).toUpperCase();
         // make rest lower case
         let restOfString = (getChoice.slice(1)).toLowerCase();
-    let playerChoice= firstLetter + restOfString
-    console.log(playerChoice.valueOf())
+    let playerChoice= firstLetter + restOfString;
+    
     
     //2.2 SecurityCheck for right input
     if(playerChoice == 'Rock' || playerChoice == 'Paper' || playerChoice == 'Scissors' )
     {
+        //console.log(`good input`);
         return playerChoice;
-        //console.log(`PlayerSelection: ${playerChoice}`);
+        
     }
     else 
     {
@@ -38,16 +40,19 @@ function playerPlay()
     
     
 }
-let playerSelection = playerPlay();
-//console.log(`${typeof playerSelection} ${playerSelection}`)
+
 
 // 3- function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
-function playRound(){
-    
+function playRound(playerSelection, computerSelection){
+    computerSelection = computerPlay();
+    playerSelection = playerPlay();
+
     if ((playerSelection == 'Rock' && computerSelection == 'Scissors') ||
     (playerSelection == 'Scissors' && computerSelection == 'Paper') ||
-    (playerSelection == 'Paper' && computerSelection == 'Rock')){
+    (playerSelection == 'Paper' && computerSelection == 'Rock'))
+    {
         console.log(`you win- player: ${playerSelection} and computer:${computerSelection}`);
+        playerScore++ ;
     }
     else if(playerSelection == computerSelection)
     {
@@ -56,33 +61,35 @@ function playRound(){
     }
     else{
         console.log(`You lose- player: ${playerSelection} and computer:${computerSelection}`);
+        computerScore++ ;
     }
 }
-playRound();
-
-
-//mportant note: you want to return the results of this function call, not console.log() them. To test this function console.log the results:
-
-/* function playRound(playerSelection, computerSelection) {
-  // your code here!
-}
-
-const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
-*/
-
 
 
 // 4- Write a NEW function called game(). Use the previous function inside of this one to play a 5 round game that keeps score and reports a winner or loser at the end. 
 
-// You have not officially learned how to “loop” over code to repeat function calls… if you already know about loops from somewhere else (or if you feel like doing some more learning) feel free to use them. If not, don’t worry! Just call your playRound function 5 times in a row. Loops are covered in the next lesson.
-
 //At this point you should be using console.log() to display the results of each round and the winner at the end.
 
-
-//Use prompt() to get input from the user. read here if need to  :https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
-
+function game()
+{
+    for(let i = 1; i <=5; i++)
+    {
+        playRound();
+        console.log(`tour ${i} : playerScore ${playerScore}`);
+    }
+    if(playerScore> computerScore)
+    {
+        console.log(`End of the game, you WIN !`)
+    }
+    else if (playerScore < computerScore)
+    {
+        console.log(`End of the game, you LOOSE !`)
+    }
+    else {
+        console.log(`End of the game, it's a TIE !`)
+    }
+}
+game()
 
 
 
